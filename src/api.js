207ef9be2756c0164d381da82bf791c28698d3b2
Diatlova-Nataliia <natasha200124@gmail.com>
2024-10-29 11:37:52 +0900

    Added .gitignore

---
+++ src/api.js
@@ -0,0 +1,23 @@
+const api = {
+    create() {
+        console.log('create');
+    },
+
+    readAll() {
+        console.log('read');
+    },
+
+    read() {
+        console.log('read');
+    },
+
+    update() {
+        console.log('read');
+    },
+
+    delete() {
+        console.log('read');
+    }
+}
+
+export default api;
